9b843c6b42d5bec4fe792c61e337b97762bf4005
Ethan <ethan_gustafson@outlook.com>
2020-05-26 06:23:02 -0400

    added divs around form & ul

--- javascript/Forms/Form.js
+++ javascript/Forms/Form.js
@@ -1,13 +1,21 @@
 class Form {
     constructor(){
+        this.div = document.createElement("div");
+        this.div.id = "formDiv";
+        document.body.appendChild(this.div);
+        
         this.textInput = document.createElement("form");
         this.textInput.id = "To-Do-Form";
         this.textInput.innerHTML = `<input id='ToDo' type='text' name='ToDo'/><input type='submit' name='Submit'/>`;
-        document.body.appendChild(this.textInput);
+        this.div.appendChild(this.textInput);
+
+        this.ulDiv = document.createElement("div");
+        this.ulDiv.id = "ulDiv";
+        document.body.appendChild(this.ulDiv)
 
         this.ul = document.createElement("ul");
         this.ul.id = "ul";
-        document.body.appendChild(this.ul);
+        this.ulDiv.appendChild(this.ul);
 
         this.handleOnSubmit();
     }
