20def1deaeb775440840b610976b450c3bd2a0e6
Ethan <ethan_gustafson@outlook.com>
2020-05-26 06:48:31 -0400

    delete button deletes to-do's

--- javascript/To-Do/To-Do.js
+++ javascript/To-Do/To-Do.js
@@ -1,22 +1,28 @@
 class ToDo{
+
+    static all = [];
+
     constructor(value){
+        this.id = document.getElementsByTagName("li").length;
         this.li = document.createElement("li");
-        this.li.id = `ToDoLi`;
+        this.li.id = 'ToDoLi';
         this.li.innerHTML = `${value}`;
-        this.ulDiv = document.getElementById("ulDiv")
+        this.ulDiv = document.getElementById("ulDiv");
         this.ulDiv.appendChild(this.li);
 
         this.deleteButton = document.createElement("button");
         this.deleteButton.id = "deleteButton";
         this.deleteButton.innerHTML = "Delete";
         this.li.appendChild(this.deleteButton);
-        handleOnSubmit();
+        this.handleOnSubmit();
+        ToDo.all.push(this);
     }
 
     handleOnSubmit = () => {
         this.deleteButton.addEventListener("click", event => {
             event.preventDefault();
-
+            this.li.remove();
+            ToDo.all.pop();
         })
     }
 }
