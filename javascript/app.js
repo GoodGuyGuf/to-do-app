100021b53e7e4d087603c5e63505dc5154049feb
Ethan <ethan_gustafson@outlook.com>
2020-05-26 06:01:54 -0400

    added a new App class, switched load order of js links in index.html

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+class App {
+    constructor(){
+        this.logo = document.createElement("h1");
+        this.logo.id = "logo";
+        this.logo.innerHTML = "<h1>To-Do</h1>";
+        document.body.appendChild(this.logo);
+    }
+}
