135740973d561068afedca5b332a8dc6ea2b5776
Ethan <ethan_gustafson@outlook.com>
2020-05-26 23:36:36 -0400

    added disclaimer

--- javascript/app.js
+++ javascript/app.js
@@ -9,6 +9,11 @@ class App {
         this.logo.innerHTML = "To-Do";
         this.div.appendChild(this.logo);
 
-        new Form
+        this.disclaimer = document.createElement("p");
+        this.disclaimer.id = "disclaimer";
+        this.disclaimer.innerHTML = "Disclaimer: To-Do's are not saved when you exit this session.";
+        this.div.appendChild(this.disclaimer);
+
+        new Form;
     }
 }
